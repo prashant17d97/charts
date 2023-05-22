790384d1f970d4a0b29009faea82bfcc98755418
prashantd97 <prashantkumar@apptunix.com>
2023-05-22 12:17:24 +0530

    documentation

--- app/src/main/java/com/prashant/barchart/BarGraphAdapter.kt
+++ app/src/main/java/com/prashant/barchart/BarGraphAdapter.kt
@@ -6,24 +6,46 @@ import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
-import android.view.View.*
+import android.view.View.GONE
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.prashant.barchart.databinding.BarViewHorizontalBinding
 import com.prashant.barchart.databinding.BarViewVerticalBinding
 
+/**
+ * BarGraphAdapter is an adapter class used for populating a RecyclerView with bar graph items.
+ * @param list The list of BarModel objects representing the data for the bar graph.
+ * @param activity The activity associated with the adapter.
+ */
 class BarGraphAdapter(private val list: List<BarModel>, val activity: Activity) :
     RecyclerView.Adapter<BarGraphAdapter.BarView>() {
 
-    inner class BarView(val binding: BarViewVerticalBinding) : RecyclerView.ViewHolder(binding.root)
+    /**
+     * BarView is a ViewHolder class representing a single item view in the RecyclerView.
+     * @param binding The view binding object for the item view.
+     */
+    inner class BarView(val binding: BarViewVerticalBinding) :RecyclerView.ViewHolder(binding.root)
 
+    /**
+     * Called when RecyclerView needs a new BarView to represent an item.
+     * @param parent The ViewGroup into which the new View will be added after it is bound to an adapter position.
+     * @param viewType The view type of the new View.
+     * @return A new instance of BarView representing the item view.
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BarView {
         val binding =
             BarViewVerticalBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return BarView(binding)
     }
 
+    /**
+     * Called by RecyclerView to display the data at the specified position.
+     * @param holder The BarView to bind the data to.
+     * @param position The position of the item within the adapter's data set.
+     */
     override fun onBindViewHolder(holder: BarView, position: Int) {
         with(holder.binding) {
             tvXAxis.text = list[position].xAxisValue
@@ -36,27 +58,51 @@ class BarGraphAdapter(private val list: List<BarModel>, val activity: Activity) 
             }
             progressHorizontal.progress = list[position].barValue
             progressHorizontal.max = 100
-            verticalView.visibility = VISIBLE.takeIf { position==0 }?:GONE
+            verticalView.visibility = VISIBLE.takeIf { position == 0 } ?: GONE
         }
     }
 
+    /**
+     * Returns the total number of items in the data set held by the adapter.
+     * @return The total number of items in the adapter.
+     */
     override fun getItemCount() = list.size
 }
 
+/**
+ * BarGraphHorizontalAdapter is an adapter class used for populating a RecyclerView with horizontal bar graph items.
+ * @param barModelList The list of BarModel objects representing the data for the horizontal bar graph.
+ * @param activity The activity associated with the adapter.
+ */
 class BarGraphHorizontalAdapter(
     private val barModelList: List<BarModel>, private val activity: Activity
-) : RecyclerView.Adapter<BarGraphHorizontalAdapter.BarView>() {
+) :RecyclerView.Adapter<BarGraphHorizontalAdapter.BarView>() {
     private var progressDrawable = ContextCompat.getDrawable(activity, R.drawable.custom_progress)
 
+    /**
+     * BarView is a ViewHolder class representing a single item view in the RecyclerView.
+     * @param binding The view binding object for the item view.
+     */
     inner class BarView(val binding: BarViewHorizontalBinding) :
         RecyclerView.ViewHolder(binding.root)
 
+    /**
+     * Called when RecyclerView needs a new BarView to represent an item.
+     * @param parent The ViewGroup into which the new View will be added after it is bound to an adapter position.
+     * @param viewType The view type of the new View.
+     * @return A new instance of BarView representing the item view.
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BarView {
         val binding =
             BarViewHorizontalBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return BarView(binding)
     }
 
+    /**
+     * Called by RecyclerView to display the data at the specified position.
+     * @param holder The BarView to bind the data to.
+     * @param position The position of the item within the adapter's data set.
+     */
     override fun onBindViewHolder(holder: BarView, position: Int) {
         with(holder.binding) {
             tvXAxis.text = barModelList[position].xAxisValue
@@ -64,13 +110,20 @@ class BarGraphHorizontalAdapter(
             Log.e("TAG", "onBindViewHolder: ${barModelList[position].barValue}")
             progressHorizontal.max = 100
 //            progressHorizontal.progressDrawable = progressDrawable
-            bottomView.visibility = VISIBLE.takeIf { position == barModelList.size - 1 }?:GONE
-
+            bottomView.visibility = VISIBLE.takeIf { position == barModelList.size - 1 } ?: GONE
         }
     }
 
+    /**
+     * Returns the total number of items in the data set held by the adapter.
+     * @return The total number of items in the adapter.
+     */
     override fun getItemCount() = barModelList.size
 
+    /**
+     * Sets the progress drawable for the horizontal bar graph.
+     * @param progressDrawable The drawable used for the progress indicator.
+     */
     fun setAttribute(
         progressDrawable: Drawable? = ContextCompat.getDrawable(
             activity, R.drawable.custom_progress
@@ -80,6 +133,9 @@ class BarGraphHorizontalAdapter(
     }
 }
 
+/**
+ * An enum representing the orientation of a bar graph.
+ */
 enum class Orientation {
     HORIZONTAL, VERTICAL
 }
--- app/src/main/java/com/prashant/barchart/MainActivity.kt
+++ app/src/main/java/com/prashant/barchart/MainActivity.kt
@@ -3,14 +3,29 @@ package com.prashant.barchart
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.prashant.barchart.databinding.ActivityMainBinding
-
+/**
+ * The MainActivity class is the main activity of the application.
+ * It extends the AppCompatActivity class and is responsible for displaying the main UI of the app.
+ */
 class MainActivity : AppCompatActivity() {
+
     private var _binding: ActivityMainBinding? = null
     private var binding = _binding
+
+    /**
+     * Called when the activity is being created.
+     * This is where you should initialize the activity and setContentView to the appropriate layout.
+     *
+     * @param savedInstanceState The saved instance state bundle containing the activity's previous state, if any.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Inflate the layout using the ActivityMainBinding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+
+        // Create a list of BarModel objects representing bar graph data
         val data = listOf(
             BarModel(barValue = 10, xAxisValue = "Mon"),
             BarModel(barValue = 20, xAxisValue = "Tue"),
@@ -31,12 +46,15 @@ class MainActivity : AppCompatActivity() {
             BarModel(barValue = 40, xAxisValue = "Sep"),
             BarModel(barValue = 30, xAxisValue = "Oct"),
             BarModel(barValue = 20, xAxisValue = "Nov"),
-            BarModel(barValue = 10, xAxisValue = "Dec"),
+            BarModel(barValue = 10, xAxisValue = "Dec")
         )
 
+        // Set up the RecyclerView and attach the BarGraphAdapter
         binding?.rvBars?.apply {
             adapter = BarGraphAdapter(data, this@MainActivity)
         }
+
+        // Set up the RecyclerView for horizontal bars and attach the BarGraphHorizontalAdapter
         binding?.rvHorizontalBars?.apply {
             adapter = BarGraphHorizontalAdapter(data, this@MainActivity)
         }
